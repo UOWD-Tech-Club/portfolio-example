837584a7d2535124caa80b0a0619f3b1c4b12640
Riaz <mohd.riaz.2002@gmail.com>
2025-01-19 16:26:56 +0400

    Fixed merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import ContactForm from "./components/ContactForm";
-
-function App() {
-  return <ContactForm />;
-=======
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import HomePage from "./pages/HomePage";
@@ -20,6 +10,5 @@ function App() {
       <Footer />
     </>
   );
->>>>>>> 00317ddbc751a1b93eb983c9f960a00b089ebe53
 }
 export default App;
