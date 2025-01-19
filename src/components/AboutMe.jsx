6de6dc77d51955544df7d3a31eba766bd6aca333
mariah <mariahkhalifa05@gmail.com>
2025-01-19 10:53:00 +0400

    Set up components, file structure, and global styles

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,7 @@
+import "./AboutMe.css";
+
+function AboutMe() {
+  return <div>About Me</div>;
+}
+
+export default AboutMe;
---
+++ src/components/MyProjects.jsx
@@ -0,0 +1,7 @@
+import "./MyProjects.css";
+
+function MyProjects() {
+  return <div>My Projects</div>;
+}
+
+export default MyProjects;
