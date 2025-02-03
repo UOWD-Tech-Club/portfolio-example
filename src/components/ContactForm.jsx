d23932e7c41eb268c543b04885c5293f771cf821
Riaz <mohd.riaz.2002@gmail.com>
2025-02-03 16:49:52 +0400

    Added Form Validation and Submit Function

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import styles from "./ContactForm.module.css";
+import validator from "validator";
 
 function ContactForm() {
   const [firstName, setFirstName] = useState("");
@@ -7,6 +8,40 @@ function ContactForm() {
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
   const [message, setMessage] = useState("");
+
+  const [emailError, setEmailError] = useState("");
+  const [phoneError, setPhoneError] = useState("");
+
+  function validateEmail(e) {
+    setEmail(e.target.value);
+
+    if (validator.isEmail(email)) {
+      setEmailError("");
+    } else {
+      setEmailError("Please enter a valid Email!");
+    }
+  }
+
+  function validatePhone(e) {
+    setPhone(e.target.value);
+
+    if (validator.isMobilePhone(phone)) {
+      setPhoneError("");
+    } else {
+      setPhoneError("Please enter a Phone Number!");
+    }
+  }
+
+  function submitForm(e) {
+    e.preventDefault();
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPhone("");
+    setMessage("");
+    setEmailError("");
+    setPhoneError("");
+  }
 
   return (
     <section className={styles.contents}>
@@ -16,6 +51,8 @@ function ContactForm() {
           Feel free to get in touch with me. I am always open to discussing new
           projects.
         </p>
+        <p className={styles.error}>{emailError}</p>
+        <p className={styles.error}>{phoneError}</p>
         <form className={styles.form}>
           <input
             className={styles.firstName}
@@ -32,13 +69,13 @@ function ContactForm() {
           <input
             className={styles.email}
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={validateEmail}
             placeholder="Email"
           />
           <input
             className={styles.phone}
             value={phone}
-            onChange={(e) => setPhone(e.target.value)}
+            onChange={validatePhone}
             placeholder="Phone Number"
           />
           <textarea
@@ -51,7 +88,7 @@ function ContactForm() {
             className={styles.submitButton}
             type="submit"
             value="Submit"
-            onClick={(e) => e.preventDefault()}
+            onClick={submitForm}
           />
         </form>
       </div>
